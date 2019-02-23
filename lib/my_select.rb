c73e0a454486af88a870c0c0ab888629e60fc096
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-23 02:54:36 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,9 @@ def my_select(collection)
   i = 0 
   new_array = []
   while i < collection.length 
-    if yield(collection[i]) == true 
+    if !yield(collection[i]) 
+      
+    else
       new_array[i] = yield(collection[i])
     end
     i += 1 
